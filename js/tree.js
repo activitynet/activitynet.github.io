c218ed63c0730e0bad2317f221cae0d986fb4183
Fabian Caba Heilbron <cabaf@localhost.localdomain>
2015-08-20 16:23:25 +0300

    Initial commit: website second version.

---
+++ js/tree.js
@@ -0,0 +1,177 @@
+var nodeId = 0;
+
+$(document).ready(function(){
+  $('iframe').hide()
+  $.ajax({
+    url: "http://ec2-52-11-203-1.us-west-2.compute.amazonaws.com/generate_treeview.php",
+    type: "POST",
+    success: function(data)
+    {
+      $('.tree').html(data);
+
+      $('.tree li:has(ul)').addClass('parent_li').find(' > span').attr('title', 'Expand this branch');
+      $('.tree li.parent_li').find(' > ul > li').hide();
+	
+	  var categories = $('#categories >ul>li');
+	  $(categories).each(function(){
+		var account = $(this).find('i').length;
+		var itemStr = $(this).find('>span').text();
+		var itemName = itemStr + "(" + account + ")";
+		$(this).find('>span').get(0).lastChild.nodeValue = itemName;
+	  });  
+	  
+      $('#categories').find(' > ul > li').show();
+
+    },
+    error:function(data){
+      //$('.tree').text(data);
+      console.log(data);
+    }
+  });    
+
+  getVideos(nodeId, 1);
+
+});
+
+// change node condition and icon
+
+$('body').on('click', '.tree li.parent_li > span', function (e) {
+  var children = $(this).parent('li.parent_li').find(' > ul > li');
+  if (children.is(":visible")) {
+    children.hide('fast');
+    $(this).attr('title', 'Expand this branch').find(' > i').addClass("glyphicon-plus-sign").removeClass("glyphicon-minus-sign");
+  } else {
+    children.show('fast');
+    $(this).attr('title', 'Collapse this branch').find(' > i').addClass("glyphicon-minus-sign").removeClass("glyphicon-plus-sign");
+  }
+  e.stopPropagation();
+});
+
+var ResultsperPage = 16;
+
+// When user clicks on a node
+
+$('body').on('click', '.tree li span', function(){
+
+  $('iframe').hide();
+  nodeId = $(this).attr('id');
+  $('.tree li span').removeClass('selected');
+  $(this).addClass('selected');
+  var page = 1;
+  getVideos(nodeId, page);
+  return false;
+});
+
+$('body').on('click', '.nextpage',function(e){
+  var thispage = $(this).attr('href');
+
+  $.ajax({
+    url:"http://ec2-52-11-203-1.us-west-2.compute.amazonaws.com/getvideos.php",
+    type:"POST",
+    data:{nodeId : nodeId, page : thispage},
+    success:function(data) {
+      $('#info a').empty();
+    var videos = jQuery.parseJSON(data);    // Take all videos from JSON.
+
+    $.each(videos.videos.video, function(i, v){
+      $('#info').append('<a class="video" href="'+ v.videoId + '"title="'+ v.title +'" data-witdh = "640" data-height = "360"  href="'+ v.location +
+	  					'"><img src="http://img.youtube.com/vi/'+ v.videoId +'/1.jpg"></a>'); 
+    });
+
+    $('.tree li span').removeClass('selected');
+    $(this).addClass('selected');
+
+    var pages = Math.ceil(videos[0].size/ResultsperPage);
+    CheckPage(thispage, pages);
+    $('#videoinfo').empty();
+  }
+});
+  return false;
+}); 
+
+$('body').on('click', '#showall',function(e){
+  $('.tree li:has(ul)').addClass('parent_li').find(' > span').attr('title', 'Collapse this branch');
+  $('.tree li.parent_li').find(' > ul > li').show('fast')
+  $('.tree li.parent_li i').addClass("glyphicon-minus-sign").removeClass("glyphicon-plus-sign");
+  $('.tree li.movie i').addClass("glyphicon-film").removeClass("glyphicon-minus-sign");
+});
+
+$('body').on('click', '#hideall', function(e){
+  $('.tree li:has(ul)').addClass('parent_li').find(' > span').attr('title', 'Expand this branch');
+  $('.tree li.parent_li').find(' > ul > li').hide('fast');
+  $('#categories').find(' > ul > li').show('fast');
+  $('.tree li.parent_li i').addClass("glyphicon-plus-sign").removeClass("glyphicon-minus-sign");  
+  $('.tree li.movie i').addClass("glyphicon-film").removeClass("glyphicon-plus-sign");   
+});
+
+
+function CheckPage(thispage, numberofpages)
+{
+  var nextpage = parseInt(thispage) + 1;
+  var prevpage = parseInt(thispage) - 1;
+
+  if(thispage == 1)
+  {
+    $('#paginationdiv p').empty().append('Page ' + thispage + ' of ' + numberofpages);
+    $('#Next_page, #First_page, #Last_page').removeClass('disabled');
+    $('#Pre_page').addClass('disabled', 'disabled');
+    $('#Pre_page a').attr('href', '#');
+    $('#Next_page a').attr('href', nextpage); 
+    $('#Last_page a').attr('href', numberofpages);
+  }
+  if(thispage > 1 && thispage < numberofpages)
+  {
+    $('#paginationdiv p').empty().append('Page ' + thispage + ' of ' + numberofpages);
+    $('#Pre_page').removeClass('disabled');
+    $('#Pre_page a').attr('href', prevpage);      
+    $('#Next_page').removeClass('disabled'); 
+    $('#Next_page a').attr('href', nextpage);
+  }
+  if(thispage == numberofpages)
+  {
+    $('#paginationdiv p').empty().append('Page ' + thispage + ' of ' + numberofpages);
+    $('#Pre_page').removeClass('disabled');
+    $('#Pre_page a').attr('href', prevpage);      
+    $('#Next_page').addClass('disabled', 'disabled');
+    $('#Next_page a').attr('href', '#');
+  }
+
+}
+
+// Global var for saving the temporal annotations of the displayed videos.
+var VIDEO_TIME = new Array();
+function getVideos(nodeId, page)
+{
+  $.ajax({
+    url:"http://ec2-52-11-203-1.us-west-2.compute.amazonaws.com/getvideos.php",
+    type:"POST",
+    data:{nodeId : nodeId},
+    success:function(data) {
+      $('#info a').empty();
+      $('#frame').hide().attr('src', "");
+      var videos = jQuery.parseJSON(data);    // Take all videos from JSON.
+      $.each(videos.videos.video, function(i, v){
+        $('#info').append('<a class="video" href="'+ v.videoId +'" title="'+ v.title +'" data-witdh = "640" data-height = "360"  href="'+ v.location + 
+						  '"><img src="http://img.youtube.com/vi/'+ v.videoId +'/1.jpg"></a>');
+		/*alert("v.videoID= " + v.videoID + "v.location= " + v.location); */
+		<!--alert("v.videoStartTime= " + v.annotations);-->
+		VIDEO_TIME[v.videoId] = new Object();
+		VIDEO_TIME[v.videoId].annotation = v,
+		VIDEO_TIME[v.videoId].title = v.title;
+		VIDEO_TIME[v.videoId].start = v.startTime;
+		VIDEO_TIME[v.videoId].end = v.endTime;
+		
+	  });
+	  
+
+      var pages = Math.ceil(videos[0].size/ResultsperPage);
+      CheckPage(page, pages);
+      $('#paginationdiv').show();
+      $('#videoinfo').empty();
+    },
+    error:function(data){
+    }
+  });
+
+
+}
---
+++ js/videoplay.js
@@ -0,0 +1,83 @@
+// When user clicks a video
+/*$('body').on('click', 'a.video', function(){
+	var myVideo = $(this).attr('href');
+	var videotitle = $(this).attr('title');
+	
+	$('#info a, #paginationdiv').hide();
+	$('#frame').attr("src", "http://www.youtube.com/embed/"+ myVideo +"?rel=0&autoplay=1&vq=hd360").show();
+	$('#videoinfo').text(videotitle);
+		
+	return false;         
+});*/
+
+function get_video_duration() {
+    return 230;	
+}
+
+$('body').on('click', 'a.video', function(){
+	var myVideo = $(this).attr('href');
+	var videotitle = $(this).attr('title');	
+	
+	/*$('#info a, #paginationdiv').hide();
+	$('#frame').attr("src", "http://www.youtube.com/embed/"+ myVideo +"?rel=0&autoplay=1&vq=hd360").show();*/
+	$('body').css('overflow','hidden');
+	$('.theme-popover-mask').fadeIn(100);
+	$('.theme-popover').slideDown(200);
+	
+	var video_duration = get_video_duration();
+	var starting_times = VIDEO_TIME[myVideo].start.split(',');
+	var ending_times = VIDEO_TIME[myVideo].end.split(',');
+	var title = VIDEO_TIME[myVideo].title;
+	//Current playing video
+	$('#frame2').attr("src", "https://www.youtube.com/v/" + myVideo + "?color=red&rel=0&start=" + starting_times[0]* video_duration +"&end=" + ending_times[0]*video_duration).show();	
+	//Video playing list
+	for (var idx=0; idx<starting_times.length; idx++) {
+		var this_start = starting_times[idx];
+		var this_end = ending_times[idx];
+		var index = idx+1;
+		imgUrl = "https://www.youtube.com/v/" + myVideo + "?color=red&rel=0&start=" + this_start * video_duration + "&end=" + this_end * video_duration;
+		$('ol.list-group').append('<li class= "list-group-item" title="Click to play"><a href="' + imgUrl + '"><div class="tag-status status1"></div><h5>' + title + '_Part' + index + '</h5></a></li>');
+
+	}
+	
+	//Play video after click the different parts in the list
+	$('.list-group-item>a').click(function(e){
+		e.preventDefault();	
+		$('#frame2').attr('src', $(this).attr("href"));
+	});
+	
+	//Close the player and mask
+	$('.theme-poptit .close').click(function(){
+		$('.theme-popover-mask').fadeOut(100);
+		$('.theme-popover').slideUp(200);
+		$('body').css('overflow','visible');
+		$('li.list-group-item').remove();
+		$('ol.list-group').removeAttr("list-group-item");
+	});	
+		
+	return false;         
+});
+
+
+
+
+/*$('body').on('click','list-group-item.tag-status',function(){
+	$('this').addClass('status2').
+	
+});
+
+$('body').on('click', '#showall',function(e){
+  $('.tree li:has(ul)').addClass('parent_li').find(' > span').attr('title', 'Collapse this branch');
+  $('.tree li.parent_li').find(' > ul > li').show('fast')
+  $('.tree li.parent_li i').addClass("glyphicon-minus-sign").removeClass("glyphicon-plus-sign");
+  $('.tree li.movie i').addClass("glyphicon-film").removeClass("glyphicon-minus-sign");
+});*/
+	
+
+/*$('a.video').click(function(){
+	var myVideo = $(this).attr('href');
+	alert("video href= " + myVideo);
+	$('.theme-popover-mask').fadeIn(100);
+	$('.theme-popover').slideDown(200);
+	$('#frame2').attr("src", "http://www.youtube.com/embed/"+ myVideo +"?rel=0&autoplay=1&vq=hd360").show();
+});*/
