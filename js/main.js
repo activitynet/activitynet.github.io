4a945d674bdac5f49eba103738e18f35c16fa243
Jinler <jinlei.wang06@gmail.com>
2015-08-30 09:42:50 +0300

    menu hover
    move the js for menu hover from html to main.js

--- js/main.js
+++ js/main.js
@@ -1,49 +1,22 @@
 jQuery(function($) {'use strict';
 	
-	/*$(window).scroll(function(event) {
+	// Navigation Scroll
+	$(window).scroll(function(event) {
 		Scroll();
-	});*/
-
-
-// Navigation Scroll		
-	/*$('.navbar-collapse ul li a').scroll(function(event) {  
-		Scroll();
-	});*/
-	
-	
-	
-/*	function Scroll() {
-		var contentTop      =   [];
-		var contentBottom   =   [];
-		var winTop      =   $(window).scrollTop();
-		var rangeTop    =   200;
-		var rangeBottom =   500;
-		$('.navbar-collapse').find('.scroll a').each(function(){
-			contentTop.push( $( $(this).attr('href') ).offset().top);
-			contentBottom.push( $( $(this).attr('href') ).offset().top + $( $(this).attr('href') ).height() );
-		})
-		$.each( contentTop, function(i){
-			if ( winTop > contentTop[i] - rangeTop ){
-				$('.navbar-collapse li.scroll')
-				.removeClass('active')
-				.eq(i).addClass('active');	
-			}
-		})
-	};*/
-
-
-// Navigation Scroll
-	/*$(window).scroll(function(event) {
-		Scroll();
-	});*/
+	});
 
 	/*$('.navbar-collapse ul li a').on('click', function() {  
 		$('html, body').animate({scrollTop: $(this.hash).offset().top - 5}, 1000);
 		return false;
 	});*/
+	$('.nav-folder ul li a').on('click', function() {  
+		$('html, body').animate({scrollTop: $(this.hash).offset().top - 5}, 1000);
+		return false;
+	});
+	
 
 	// User define function
-/*	function Scroll() {
+	function Scroll() {
 		var contentTop      =   [];
 		var contentBottom   =   [];
 		var winTop      =   $(window).scrollTop();
@@ -57,10 +30,11 @@ jQuery(function($) {'use strict';
 			if ( winTop > contentTop[i] - rangeTop ){
 				$('.navbar-collapse li.scroll')
 				.removeClass('active')
-				.eq(i).addClass('active');	
+				.eq(i).addClass('active');			
 			}
 		})
-	};*/
+	};
+	
 
 	$('#tohash').on('click', function(){
 		$('html, body').animate({scrollTop: $(this.hash).offset().top - 5}, 1000);
@@ -74,6 +48,13 @@ jQuery(function($) {'use strict';
 		 });
 
 	 	$(this).closest('.panel-heading').toggleClass('active');
+	});
+	
+	//navbar
+	$(".navbar-nav >li").hover(function(){
+		$(this).find(".nav-folder").css("display","block");
+		},function(){
+		$(this).find(".nav-folder").css("display","none");
 	});
 
 	//Slider
