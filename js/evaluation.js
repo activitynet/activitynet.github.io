d5dca30f5c11de1d2c9f392dea63583230d17607
Jinler <jinlei.wang06@gmail.com>
2015-10-24 15:03:55 +0300

    Remove the preview box in evaluate page
    Remove the preview box in evaluate page

--- js/evaluation.js
+++ js/evaluation.js
@@ -175,8 +175,10 @@ function print_classification_content() {
 	  		elements: {
 	    		selectors: ['#file_to_upload']
 	  		}
-	};
+	   };
+	
     $("#file_to_upload").fileinput({
+		showPreview:false,
         maxFileCount: 1,
         uploadAsync: false,
         uploadUrl:serverurl + "/upload.php",
