ece1d175026581405695f207e54ff95deaeb9d1d
Jinler <jinlei.wang06@gmail.com>
2015-10-26 11:34:48 +0300

    Fix the bug when upload
    Fix the bug for uploading error format while processing bar keep running

--- js/evaluation.js
+++ js/evaluation.js
@@ -210,7 +210,7 @@ function print_classification_content() {
         }
     }).on('filebatchpreupload', function(event, data, id, index) {
 	  $('.kv-upload-progress').remove();	
-      $('#kv-success-2').html('<div style="background:url(images/process_48.gif) no-repeat center center; width=100%; height:107px;"><div class="section-title text-center" style="padding-top:86px"><span >Uploading and evaluating JSON file … This might take a few minutes</span></div></div>').show();
+      $('#kv-success-2').html('<div style="background:url(images/process_48.gif) no-repeat center center; width=100%; height:107px;"><div class="section-title text-center" style="padding-top:86px"><span >Uploading and evaluating JSON file … This might take a few minutes</span></div></div>');
     }).on('filebatchuploadsuccess', function(event, data) {
       var out = '';
       var result_url = data.response[0];
