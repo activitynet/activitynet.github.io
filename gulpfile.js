39a638e354ea1a1947645afdc8778bbe0e096746
Erik Dao <erikddao@gmail.com>
2017-12-07 02:36:01 +0300

    New challenge homepage

---
+++ gulpfile.js
@@ -0,0 +1,19 @@
+/**
+ * file: gulpfile.js
+ * @file
+ * Gulp file to compile the SASS files into CSS.
+ */
+'use strict';
+
+var gulp = require('gulp');
+var sass = require('gulp-sass');
+
+gulp.task('sass', function() {
+	gulp.src('./sass/**/main.scss')
+		.pipe(sass().on('error', sass.logError))
+		.pipe(gulp.dest('./challenges/2018/css'));
+});
+
+gulp.task('default', function() {
+	gulp.watch('./sass/**/*.scss', ['sass']);
+})
