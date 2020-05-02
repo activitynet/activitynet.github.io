a9f52873d1bd5eb02b0059abd59cdab61628dacd
caba <caba@adobe.com>
2020-05-02 16:09:51 -0700

    Evaluation server for 2020 challenge enabled.

--- challenges/2020/js/evaluation.js
+++ challenges/2020/js/evaluation.js
@@ -1,4 +1,4 @@
-SERVERURL = 'http://ec2-52-25-205-214.us-west-2.compute.amazonaws.com/challenge19/'
+SERVERURL = 'http://ec2-35-167-219-180.us-west-2.compute.amazonaws.com/challenge20/'
 var login_data;
 var leadership_data;
 var EMAIL;
@@ -7,15 +7,15 @@ var PASSWORD;
 var fullname;
 var FIRSTNAME;
 var LASTNAME;
-var serverurl = "http://ec2-52-25-205-214.us-west-2.compute.amazonaws.com/evaluation_server";
-var files = "http://ec2-52-25-205-214.us-west-2.compute.amazonaws.com/files";
-var TASKID = 1;
+var serverurl = "http://ec2-35-167-219-180.us-west-2.compute.amazonaws.com/evaluation_server";
+var files = "http://ec2-35-167-219-180.us-west-2.compute.amazonaws.com/files";
+var TASKID = 2;
 var USERID;
 var fullname;
 var PUBLIC = 'true';
 $(function() {
 
-  print_results()
+  // print_results()
 
   $('#login-button').on('click', function() {
     var email = document.getElementById("email").value;
@@ -157,8 +157,8 @@ function print_logged(){
     }
   });
 }
-json_file_classification = "http://ec2-52-25-205-214.us-west-2.compute.amazonaws.com/files/example_classification.json";
-json_file_detection = "http://ec2-52-25-205-214.us-west-2.compute.amazonaws.com/files/example_detection.json";
+json_file_classification = "http://ec2-35-167-219-180.us-west-2.compute.amazonaws.com/files/example_classification.json";
+json_file_detection = "http://ec2-35-167-219-180.us-west-2.compute.amazonaws.com/files/example_detection.json";
 
 function load_example_formats() {
 
@@ -170,13 +170,13 @@ function load_example_formats() {
   //   }
   // });
 
-    $.ajax({
-    url:SERVERURL + "/submission_formats/example_proposals.html",
-    type:"POST",
-    success: function(html) {
-      $("#example-proposals").html(html);
-    }
-  });
+  //   $.ajax({
+  //   url:SERVERURL + "/submission_formats/example_proposals.html",
+  //   type:"POST",
+  //   success: function(html) {
+  //     $("#example-proposals").html(html);
+  //   }
+  // });
 
     $.ajax({
     url:SERVERURL + "/submission_formats/example_localization.html",
@@ -224,8 +224,8 @@ function print_results_format(html) {
   // var untrimmed_ = "<h3>Submit Your Results</h3><h4>Untrimmed Video Classification</h4><p>Please format your results as illustrated in the example below. You can also download this <a href='%s' target='_blank' download> example submission file</a>. For anonymous submissions please contact us at fabian.caba@kaust.edu.sa</p><pre><code id=example-untrimmed></code></pre>"
   // untrimmed_ = sprintf(untrimmed_, SERVERURL + "/submission_formats/example_untrimmed.json");
 
-  var proposals_ = "<h3>Submit Your Results</h3><h4>Temporal Action Proposals</h4><p>Please format your results as illustrated in the example below. You can also download this <a href='%s' target='_blank' download> example submission file</a>. For anonymous submissions please contact us at fabian.caba@kaust.edu.sa</p><pre><code id=example-proposals></code></pre>"
-  proposals_ = sprintf(proposals_, SERVERURL + "/submission_formats/example_proposals.json");
+  // var proposals_ = "<h3>Submit Your Results</h3><h4>Temporal Action Proposals</h4><p>Please format your results as illustrated in the example below. You can also download this <a href='%s' target='_blank' download> example submission file</a>. For anonymous submissions please contact us at fabian.caba@kaust.edu.sa</p><pre><code id=example-proposals></code></pre>"
+  // proposals_ = sprintf(proposals_, SERVERURL + "/submission_formats/example_proposals.json");
 
   var localization_ = "<h3>Submit Your Results</h3><h4>Temporal Action Localization</h4><p>Please format your results as illustrated in the example below. You can also download this <a href='%s' target='_blank' download> example submission file</a>. For anonymous submissions please contact us at fabian.caba@kaust.edu.sa</p><pre><code id=example-localization></code></pre>"
   localization_ = sprintf(localization_, SERVERURL + "/submission_formats/example_localization.json");
@@ -244,25 +244,28 @@ function print_results_format(html) {
   //spatiotemporal_full_ = sprintf(spatiotemporal_full_, SERVERURL + "/submission_formats/example_spatiotemporal.csv");
 
   //html = sprintf(html, untrimmed_, trimmed_, proposals_, localization_, captioning_);
-  html = sprintf(html, proposals_, localization_, captioning_, trimmed_, spatiotemporal_, spatiotemporal_full_);
+  //html = sprintf(html, proposals_, localization_, captioning_, trimmed_, spatiotemporal_, spatiotemporal_full_);
+  html = sprintf(html, localization_, captioning_, trimmed_, spatiotemporal_, spatiotemporal_full_);
   return html;
 }
 
 function print_classification_content() {
- 	TASKID = 1;
+ 	TASKID = 2;
 
-    var html = '<div id="evaluate" >'+
+    var html = '<h3> Submission Panel: <span style="color:red;"> One submission per team per week (per task)</span>  </h3>'
+    html += '<div id="evaluate" >'+
           '<div class="row"><div class="col-md-12"><div class="panel with-nav-tabs panel-default">'+
             '<div class="panel-heading">'+//<span class="nav-tab-title pull-right">Upload your results </span>'+
-            '<ul class="nav nav-tabs"><li class="active"><a href="#proposals" data-toggle="tab">&nbsp;Proposals</a></li>'+
-            '<li><a href="#localization" data-toggle="tab">&nbsp;Temporal Localization</a></li>'+
+            '<ul class="nav nav-tabs">'+
+            // '<li class="active"><a href="#proposals" data-toggle="tab">&nbsp;Proposals</a></li>'+
+            '<li class="active"><a href="#localization" data-toggle="tab">&nbsp;Temporal Localization</a></li>'+
             '<li><a href="#captioning" data-toggle="tab">&nbsp;Captioning</a></li>'+
             '<li><a href="#trimmed" data-toggle="tab">&nbsp;Kinetics</a></li>'+
             '<li><a href="#spatiotemporal" data-toggle="tab">&nbsp;AVA (Actions)</a></li>'+
             '<li><a href="#spatiotemporal_full" data-toggle="tab">&nbsp;AVA (Speaker)</a></li></ul></div>'+
             '<div class="panel-body"><div class="tab-content">'+
-              '<div class="tab-pane active" id="proposals">%s</div>'+
-              '<div class="tab-pane" id="localization">%s</div>'+
+              // '<div class="tab-pane active" id="proposals">%s</div>'+
+              '<div class="tab-pane active" id="localization">%s</div>'+
               '<div class="tab-pane" id="captioning">%s</div>'+
               '<div class="tab-pane" id="trimmed">%s</div>'+
               '<div class="tab-pane" id="spatiotemporal">%s</div>'+
@@ -356,13 +359,14 @@ function print_classification_content() {
   //    		$('#kv-success-2').hide();
 		// 	$('#kv-error-2').hide();
 		// }else 
-    if(currid == '#proposals'){
-      //$(this).addClass("active").siblings().removeClass("active");
-      TASKID = 1;
-      $('#file_to_upload').fileinput('clear');
-        $('#kv-success-2').hide();
-      $('#kv-error-2').hide();
-    }else if(currid == '#localization'){
+    // if(currid == '#proposals'){
+    //   //$(this).addClass("active").siblings().removeClass("active");
+    //   TASKID = 1;
+    //   $('#file_to_upload').fileinput('clear');
+    //     $('#kv-success-2').hide();
+    //   $('#kv-error-2').hide();
+    // }else 
+    if(currid == '#localization'){
       //$(this).addClass("active").siblings().removeClass("active");
       TASKID = 2;
       $('#file_to_upload').fileinput('clear');
@@ -409,7 +413,7 @@ function print_results() {
     //                    <h3>Congratulations to all the winners. We hope to see you all at CVPR!</h3>
     //                    <h3>Leadership board</h3>`
 
-    var header_html = '<h3> ActivityNet Challenge 2019 Evaluation Server'
+    var header_html = '<h3> ActivityNet Challenge 2020 Evaluation Server </h3> </br> <h3> Account Entries </h3>'
 
 
     $("#leader_div").append(header_html);
@@ -417,15 +421,16 @@ function print_results() {
     var html = '<div id="leaderboard" >'+
           '<div class="row"><div class="col-md-12"><div class="panel with-nav-tabs panel-default">'+
             '<div class="panel-heading">'+//<span class="nav-tab-title pull-right">Leadership board </span>'+
-            '<ul class="nav nav-tabs"><li class="active"><a href="#leaderboard_proposals" data-toggle="tab">&nbsp;Proposals</a></li>'+
-            '<li><a href="#leaderboard_localization" data-toggle="tab">&nbsp;Temporal Localization</a></li>'+
+            '<ul class="nav nav-tabs">'+
+            // '<li class="active"><a href="#leaderboard_proposals" data-toggle="tab">&nbsp;Proposals</a></li>'+
+            '<li class="active"><a href="#leaderboard_localization" data-toggle="tab">&nbsp;Temporal Localization</a></li>'+
             '<li><a href="#leaderboard_captioning" data-toggle="tab">&nbsp;Captioning</a></li>'+
             '<li><a href="#leaderboard_trimmed" data-toggle="tab">&nbsp;Kinetics</a></li>'+
             '<li><a href="#leaderboard_spatiotemporal" data-toggle="tab">&nbsp;AVA (Actions)</a></li>'+
             '<li><a href="#leaderboard_spatiotemporal_full" data-toggle="tab">&nbsp;AVA (Speaker)</a></li></ul></div>'+
             '<div class="panel-body"><div class="tab-content">'+
-              '<div class="tab-pane active" id="leaderboard_proposals"></div>'+
-              '<div class="tab-pane" id="leaderboard_localization"></div>'+
+              // '<div class="tab-pane active" id="leaderboard_proposals"></div>'+
+              '<div class="tab-pane active" id="leaderboard_localization"></div>'+
               '<div class="tab-pane" id="leaderboard_captioning"></div>'+
               '<div class="tab-pane" id="leaderboard_trimmed"></div>'+
               '<div class="tab-pane" id="leaderboard_spatiotemporal"></div>'+
@@ -434,16 +439,16 @@ function print_results() {
 
     $("#leader_div").append(html);
     $("#leader_div").show();
-  load_leaderboard(actionstatus='proposals');
+  load_leaderboard(actionstatus='localization');
   $('#leaderboard li').on('click', function(){
     var currid = $(this).children(':first').attr('href');
 
-    if(currid == "#leaderboard_proposals"){
-      $('#myTable tbody>tr').empty();
-      actionstatus = "proposals";
-      load_leaderboard(actionstatus);
-    }
-    else if(currid == "#leaderboard_localization"){
+    // if(currid == "#leaderboard_proposals"){
+    //   $('#myTable tbody>tr').empty();
+    //   actionstatus = "proposals";
+    //   load_leaderboard(actionstatus);
+    // }
+    if(currid == "#leaderboard_localization"){
       $('#myTable tbody>tr').empty();
       actionstatus = "localization";
       load_leaderboard(actionstatus);
@@ -474,11 +479,11 @@ function print_results() {
 function load_leaderboard(STATUS){
 	var typecontent;
  
-  if(STATUS == "proposals"){
-    typecontent = "Temporal Action Proposals";
-    var performance = 'AUC'
-  }
-  else if(STATUS == "localization"){
+  // if(STATUS == "proposals"){
+  //   typecontent = "Temporal Action Proposals";
+  //   var performance = 'AUC'
+  // }
+  if(STATUS == "localization"){
     typecontent = "Temporal Action Localization";
     var performance = 'Avg. mAP'
   }
@@ -514,7 +519,7 @@ function load_leaderboard(STATUS){
 
 	$("#table-content").html(html);
 
-  var task_mapping = {'proposals': 1, 'localization': 2, 'captioning': 3, 'trimmed': 4, 'spatiotemporal': 5, 'spatiotemporal_full': 6}
+  var task_mapping = {'localization': 2, 'captioning': 3, 'trimmed': 4, 'spatiotemporal': 5, 'spatiotemporal_full': 6}
   get_best_result(USERID, task_mapping[STATUS])
 
 }
